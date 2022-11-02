3abef722b5b52730f4f2d9650f978a7ea024aa99
Palmese Davide Mattia <palmesedavidemattia@gmail.com>
2022-11-02 15:15:47 +0100

    first commit

---
+++ src/main/java/com/example/lynxpratica/LynxPraticaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lynxpratica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LynxPraticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LynxPraticaApplication.class, args);
+	}
+
+}
