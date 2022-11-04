48901d5f0de01b49f6d9240151e6b6667d71e4bc
Vincenzo Ventriglia <vincenzo.ventriglia@lynx.italy>
2022-11-04 09:39:26 +0100

    fix

--- src/main/java/com/example/lynxpratica/controllers/RestController.java
+++ src/main/java/com/example/lynxpratica/controllers/RestController.java
@@ -1,7 +1,6 @@
 package com.example.lynxpratica.controllers;
 
 import java.math.BigDecimal;
-import java.security.PublicKey;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/example/lynxpratica/entities/Category.java
+++ src/main/java/com/example/lynxpratica/entities/Category.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -30,7 +31,7 @@ public class Category {
 	@Column(name="category")
 	private String name;
 	
-	@OneToMany(mappedBy = "category")
+	@OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
 	private List<Book> book;
 
 }
